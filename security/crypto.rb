c5c8f1fcb933efccb10cc491ffe5b7b4719650b7
bladow <garrett.bladow@gmail.com>
2013-06-02 13:52:16 +0000

    Fixed the crypt library Added new script to create secure keys Developed functional test

--- security/crypto.rb
+++ security/crypto.rb
@@ -1,36 +1,28 @@
 require 'openssl'
-require 'base64'  # he had a typo here
+require 'base64' 
 class STD
   class Crypt
+    # Initializer Sets location to key pairs for PGP encryption
     def initialize data_path
       @data_path = data_path
-      @private   = get_key 'id_rsa'
-      @public    = get_key 'id_rsa.pub'
+      @private   = get_key '/home/ec2-user/security/private.secure.pem'
+      @public    = get_key '/home/ec2-user/security/public_key.pem'
     end
  
+    # Encrypt the string and output it as a BASE64 encoded blob
     def encrypt_string message
       Base64::encode64(@public.public_encrypt(message)).rstrip
     end
  
+    # Decrypt the string as plain text ascii
     def decrypt_string message
       @private.private_decrypt Base64::decode64(message)
     end
  
-    def self.generate_keys data_path
-      rsa_path = File.join(data_path, 'rsa')
-      privkey  = File.join(rsa_path, 'id_rsa')
-      pubkey   = File.join(rsa_path, 'id_rsa.pub')
-      unless File.exists?(privkey) || File.exists?(pubkey)
-        keypair  = OpenSSL::PKey::RSA.generate(2048)
-        Dir.mkdir(rsa_path) unless File.exist?(rsa_path)
-        File.open(privkey, 'w') { |f| f.write keypair.to_pem } unless File.exists? privkey
-        File.open(pubkey, 'w') { |f| f.write keypair.public_key.to_pem } unless File.exists? pubkey
-      end
-    end
- 
     private
+    # Load the keys into memory
     def get_key filename
-      OpenSSL::PKey::RSA.new File.read(File.join(@data_path, 'rsa', filename))
+      OpenSSL::PKey::RSA.new File.read(filename)
     end
   end
 end
--- security/keyCreate.rb
+++ security/keyCreate.rb
@@ -1,15 +1,24 @@
-require 'rubygems'
-require 'openpgp'
+require 'openssl'
 
-gpg = OpenPGP::Engine::GnuPG.new(:homedir => '.')
-key_id = gpg.gen_key({
-	:key_type	=> 'DSA',
-	:key_length	=> 1024,
-	:subkey_type	=> 'ELG-E',
-	:subkey_length	=> 1024,
-	:name		=> 'Civic Hack STD',
-	:comment	=> nil,
-	:email		=> 'admin@civichackstd.org',
-	:passphrase	=> 'STD Secrets',
-	:no_verbose	=> false,
-})
+# Create the public/private key pair with 2048 bit RSA encryption
+key = OpenSSL::PKey::RSA.new 2048
+
+# write out the private key
+open 'private_key.pem', 'w' do |io| 
+	io.write key.to_pem 
+end
+#TODO make sure this private key is securily stored
+# Wtire out the public key to disk
+open 'public_key.pem', 'w' do |io| 
+	io.write key.public_key.to_pem 
+end
+
+# Protect the private key
+cipher = OpenSSL::Cipher::Cipher.new 'AES-128-CBC'
+pass_phrase = 'civichack' # Password to protect the private key
+
+key_secure = key.export cipher, pass_phrase
+
+open 'private.secure.pem', 'w' do |io|
+	io.write key_secure
+end
